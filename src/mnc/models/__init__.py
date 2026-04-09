b80ae06a53be2debd4deae4a7f0f3fa433f6b7b5
Kỳ <40734986+itskyf@users.noreply.github.com>
2026-04-09 12:23:44 +0700

    feat/MA-04: Label description encoding from ICD ontology

--- src/mnc/models/__init__.py
+++ src/mnc/models/__init__.py
@@ -1,9 +1,15 @@
 """Model components for ViTransICD."""
 
+from mnc.models.ontology_label_encoder import (
+    OntologyLabelEncoder,
+    OntologyLabelEncoderOutput,
+)
 from mnc.models.sea_lion_encoder import SeaLionEncoder, SeaLionEncoderOutput
 from mnc.models.transicd_head import TransICDHead, TransICDHeadOutput
 
 __all__ = [
+    "OntologyLabelEncoder",
+    "OntologyLabelEncoderOutput",
     "SeaLionEncoder",
     "SeaLionEncoderOutput",
     "TransICDHead",
